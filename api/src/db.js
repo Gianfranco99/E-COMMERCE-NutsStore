04122a54992a1ea457a37c506e82c2c3dbac24e7
aylenalderete <aylenalderete@hotmail.com>
2020-12-09 11:28:27 -0300

    modelos product y category

--- api/src/db.js
+++ api/src/db.js
@@ -31,9 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Product } = sequelize.models;
-
+const { Category } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Product.belongsToMany(category)
+Category.belongsToMany(product)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  const Product = sequelize.define('product', {
+  const Category = sequelize.define('category', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -12,17 +12,5 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.BLOB, //para guardar imagenes en base64
-      allownull: false
-    }
   });
 };
