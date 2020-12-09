871ad5975bed354482846489bc49c10fe3258b15
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2020-12-09 16:55:23 -0300

    correcion

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,6 +1,8 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
+
+
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
@@ -9,15 +11,24 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 server.get("/products/categoria/:nombreCat",function(req,res,next){
-
+	Product.findAll()
+	.then(products => {
+		res.send(products);
+	})
+	.catch(next);
 })
 
 server.post("/products",function(req,res,next){
-
+ Product.create(req.body)
+.then(product => res.status(201).send(product))
 })
 
 server.put("/products/:id",function(req,res,next){
-
+	Product.update(req.body,{
+        where :{
+            id:req.params.id
+        }
+    })
 })
 
 server.delete("/products/:id",function(req,res,next){
