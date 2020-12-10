bb629b19e26acfc9d9ab87cc24462427b77bee8e
Carla <carlaellyana1107@gmail.com>
2020-12-09 22:02:58 -0500

    searchBar

---
+++ client/src/components/searchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchBar() {
+  
+  return (
+    <div>
+      <input type="text" placeholder="Search products..." />
+      <button>Buscar</button>
+    </div>
+  );
+}
