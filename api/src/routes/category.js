86f0bce7390e8ba1e283340298f10c1d707b8fb4
unknown <jorge_a.correa@hotmail.com>
2020-12-11 09:41:06 -0300

    actualizado

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -15,56 +15,56 @@ server.post('/category', (req,res,next)=>{
 	.catch((error) => res.status(412).send(error));
   })
 
-  //Modificar Categoría
-  server.put('/products/category/:id', (req, res)=>{
-    Category.update(req.body,{
-      where:{
-        id:req.params.id
-      }
-    })
-    .then(category => res.status(202).send(category))
-  })
+  // //Modificar Categoría
+  // server.put('/products/category/:id', (req, res)=>{
+  //   Category.update(req.body,{
+  //     where:{
+  //       id:req.params.id
+  //     }
+  //   })
+  //   .then(category => res.status(202).send(category))
+  // })
 
   //Agrega la categoría al producto.
-  server.post('/products/:idProducto/category/:idCategoria', (req, res) => {
-    console.log('pasoporaki')
-	const PROD = await products.findByPk(idProducto);
-	const CAT = await category.findByPk(idCategoria);
-	 PROD.addCategories(CAT.dataValues.id)
+//   server.post('/products/:idProducto/category/:idCategoria', (req, res) => {
+//     console.log('pasoporaki')
+// 	const PROD = await products.findByPk(idProducto);
+// 	const CAT = await category.findByPk(idCategoria);
+// 	 PROD.addCategories(CAT.dataValues.id)
 
-.then((products) => {
- // console.log(product)
- res.status(200).json(products);
-  })
-})
+// .then((products) => {
+//  // console.log(product)
+//  res.status(200).json(products);
+//   })
+// })
 
   
 
-  //Elimina la categoría al producto
-  server.delete('/products/:idProducto/category/:idCategoria', (req, res) => {
-    	let { idProducto, idCategoria } = req.params;
-        Product.findOne({
-          where: {
-          id: idProducto,
-          },
-        })
-          .then((product) => {
-          if (!product) {
-            return res
-            .status(400)
-            .json({ message: "No se encontraron productos con ese id." });
-          }
-          product.removeCategories(idCategoria);
-          res.status(201).json({ message: "Categoria borrada" });
-          })
-          .catch((err) => err);
-  })
+  // //Elimina la categoría al producto
+  // server.delete('/products/:idProducto/category/:idCategoria', (req, res) => {
+  //   	let { idProducto, idCategoria } = req.params;
+  //       Product.findOne({
+  //         where: {
+  //         id: idProducto,
+  //         },
+  //       })
+  //         .then((product) => {
+  //         if (!product) {
+  //           return res
+  //           .status(400)
+  //           .json({ message: "No se encontraron productos con ese id." });
+  //         }
+  //         product.removeCategories(idCategoria);
+  //         res.status(201).json({ message: "Categoria borrada" });
+  //         })
+  //         .catch((err) => err);
+  // })
 
-  //eliminar Categoria
-  server.delete('/products/category/:id',(req, res) => {
-    Category.destroy({
-      where:{
-        id : req.params.id
-      }
-    })
-  })
+  // //eliminar Categoria
+  // server.delete('/products/category/:id',(req, res) => {
+  //   Category.destroy({
+  //     where:{
+  //       id : req.params.id
+  //     }
+  //   })
+  // })
