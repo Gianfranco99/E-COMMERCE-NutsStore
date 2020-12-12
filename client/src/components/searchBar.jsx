cfcbbb6ddbb31fdb979b4ad4d4f4b42bb8f16793
Carla <carlaellyana1107@gmail.com>
2020-12-12 09:05:32 -0500

    avance en SearchBar

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-export default function SearchBar() {
-  
-  return (
-    <div>
-      <input type="text" placeholder="Search products..." />
-      <button>Buscar</button>
-    </div>
-  );
+// export default function SearchBar() {
+//   const search = (props)=> {
+    
+//   }
+//   return (
+//     <div>
+//       <form>
+//       <input type="text" placeholder="Search products..." />
+//       <button type="submit" onClick={search}>Buscar</button>
+//     </form>
+//     </div>
+    
+//   );
+// }
+
+class ConnectedForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+  }
+  render() {
+    //const { search } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button type="submit">BUSCAR</button>
+      </form>
+    );
+  }
 }
