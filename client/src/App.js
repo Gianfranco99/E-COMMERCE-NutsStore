906932b7814436340e99f91312c7a693d0a03fc1
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2020-12-17 00:43:40 -0300

    for each

--- client/src/App.js
+++ client/src/App.js
@@ -15,15 +15,17 @@ function App() {
     .then(response  => response.json())
     .then((respuesta) =>{
       console.log(respuesta)
-      const product = {
-        name: respuesta.name,
-        description: respuesta.description,
-        price: respuesta.price,
-        stock: respuesta.stock,
-        id:respuesta.id
-      };
-      setProduct(oldProducts => [...oldProducts, product])
-    }).catch(err => console.log(err + "error"))
+      if(respuesta !== undefined){
+        respuesta.forEach((product) => {
+          const producto = {
+            name: product.name,
+            description: product.description,
+            price: product.price,
+            stock: product.stock,
+            id:product.id
+          }
+          setProduct(oldProducts => [...oldProducts, producto])
+        })
   
     
   }
