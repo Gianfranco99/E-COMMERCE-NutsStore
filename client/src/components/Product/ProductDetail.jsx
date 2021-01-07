91e4ec154227d05a6b60804ece5ca67842cdef7b
Harold Revelo <haroldrevelo@gmail.com>
2021-01-07 00:48:48 -0500

    stock disponibilidad

--- client/src/components/Product/ProductDetail.jsx
+++ client/src/components/Product/ProductDetail.jsx
@@ -13,7 +13,7 @@ export default function ProductDetail () {
     
       return(
         <div className="container">
-          
+          { product.stock > 0 ? (
               <div className="details">
                 <div className="big-img">
                   <img src={product.image} alt=""/> 
@@ -21,11 +21,12 @@ export default function ProductDetail () {
   
                 <div className="box">
                   <div className="row">
-                    <h2>{product.name}</h2>
-                    <span>${product.price}</span>
+                    <h2>{product.name}</h2>                    
                   </div>
+                  <h3>${product.price}</h3>
   
                   <p>{product.description}</p>
+                  <label>Stock</label>
                   <p>{product.stock}</p>
   
                   {/* <Details images={item.src} tab={handleTab} /> */}
@@ -35,8 +36,30 @@ export default function ProductDetail () {
   
                 </div>
               </div>
-            
-          
+
+          ) : (
+
+              <div className="details">
+                <div className="big-img">
+                  <img src={product.image} alt=""/> 
+                </div>
+  
+                <div className="box">
+                  <div className="row">
+                    <h2>{product.name}</h2>
+                  </div>
+                  <h3>${product.price}</h3>
+  
+                  <p>{product.description}</p>
+                  <label>Stock</label>
+                  <p>{product.stock}</p>
+  
+                  {/* <Details images={item.src} tab={handleTab} /> */}
+                  <label>Producto no disponible</label>  
+                </div>
+              </div>
+            )
+        }
         </div>
       );
     
