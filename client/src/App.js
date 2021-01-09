9a850d0bebd9c6e7618e4e1c91d1c406dc92b14f
aylenalderete <aylenalderete@hotmail.com>
2021-01-09 01:06:33 -0300

    autenticacion

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Route exact path='/micuenta' component={MiCuenta}/>
         <GuestRoute exact path='/registro' component={Registrarse}/>    
         <GuestRoute exact path='/carrito' component={Carrito}/>    
-        <AuthRoute exact path='/admin' component={Admin}/>
+        <GuestRoute exact path='/admin' component={Admin}/>
         <AuthRoute exact path='/admin2' component={Admin2}/>
         <GuestRoute exact path='/productDetail' component={ProductDetail}/>
   </Router>
