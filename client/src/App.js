d8d8cf196312d75631b259af30a0b9d2e7c170e8
Harold Revelo <haroldrevelo@gmail.com>
2021-01-11 13:39:21 -0500

    Carrito completo

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Carrito from "./components/Cart/Carrito"
 import ProductDetail from './components/Product/ProductDetail';
 import GuestRoute from './containers/GuestRoute';
 import AuthRoute from './containers/AuthRoute';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
--- client/src/components/Cart/Carrito.jsx
+++ client/src/components/Cart/Carrito.jsx
@@ -1,54 +1,143 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import "./Carrito.css"
 import imageneliminar from "../../assets/cruz.png"
 import {useSelector,useDispatch} from "react-redux";
 import Product from "../Product/Product"
-import {increment,decrement,cleanCart,removeItemCart} from '../../redux/actions/actions'
+import {addProductCart, removeProductFromCart, removeProductsFromCart} from '../../redux/actions/actions';
+import Row from 'react-bootstrap/Row';
+
+
 function Carrito() {
 const carrito = useSelector(state => state.productCart)
+console.log('este es el carrito', carrito)
 const cantidad = useSelector(state => state.count)
 const dispatch = useDispatch()
 let contador = 0;
 
 
   return (
-    <div>
-        <div className= "img-nosotros">
-            <p className= "titulo-carrito">Carrito</p>
-        </div>
-        <div className= "div-table">
-            <table>
-                <tr className= "titulos-tabla">
-                    <th>NOMBRE DEL PRODUCTO</th>
-                    <th>PRECIO</th>
-                    <th>CANTIDAD</th>
-                    <th>SUBTOTAL</th>
-                    <th>ELIMINAR</th>
-                </tr>
-                {carrito.map((m, i) =>(
-                        <tr>
-                            <td>{m.id}</td>
-                            <td>{m.name}</td>
-                            <td>{m.price}</td>
-                            <td>
-                                <button onClick={() => cantidadProductos()}>+</button> 
-                                <input value={contador}></input> 
-                                <button  onClick={() => restar()}>-</button>
-                            </td>
-                            <td>SUBTOTAL</td>
-                            <td>
-                                <button onClick={() => dispatch(removeItemCart({id : m.id}))}>x</button>
-                            </td>
-                        </tr>
-                    ))}
-                    {/* <tr> <td><img className= "imageneliminar" src= {imageneliminar}></img></td> </tr> */}
-              
-                 
-            </table>
-            <div><button onClick={()=>dispatch(cleanCart())}>limpiar carrito</button></div>
-        </div>
+      <Fragment>
+          {carrito.length === 0 ? (
+        <Row className="justify-content-center">
+          <h4>No hay productos en el carrito</h4>
+        </Row>
+        ) : (
+            <Fragment>
+          <div className="card shopping-cart">
+            <div className="card-header  text-primary">
+              <i className="fa fa-shopping-cart" aria-hidden="true"></i>{' '}
+              Shopping cart
+              <div className="clearfix"></div>
+            </div>
+            <div className="card-body">
+              {carrito.map(({ payload, quantity }, i) => (
+                <Fragment key={i}>
+                  <div className="row">
+                    <div className="col-12 col-sm-12 col-md-2 text-center">
+                      <Link to={`/productDetail/${payload.id}`}>
+                        <img
+                          className="img-responsive cart-img-obj-fit"
+                          src={payload.image}
+                          alt="prewiew"
+                          width="120"
+                          height="80"
+                        />
+                      </Link>
+                    </div>
+                    <div className="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
+                      <h4 className="product-name">{payload.name}</h4>
+                      <p>{payload.description}</p>
+                    </div>
+                    <div className="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
+                      <div
+                        className="col-3 col-sm-3 col-md-6 text-md-right"
+                        style={{ paddingTop: 5 }}
+                      >
+                        <h5>
+                          <strong>${payload.price.toFixed(2)}</strong>
+                        </h5>
+                      </div>
+                      <div className="col-4 col-sm-4 col-md-4">
+                        <div className="quantity">
+                          <button
+                            onClick={() => dispatch(addProductCart(payload))} //() => dispatch(addProductCart(props))
+                            className="plus"
+                          >
+                            +
+                          </button>
+                          <span className="quantity-number">{quantity}</span>
+                          <button
+                            onClick={() => dispatch(removeProductFromCart(payload))}
+                            className="minus"
+                          >
+                            -
+                          </button>
+                        </div>
+                      </div>
+                      <div className="col-2 col-sm-2 col-md-2 text-right">
+                        <button
+                          onClick={() => dispatch(removeProductsFromCart(payload))}
+                          type="button"
+                          className="btn btn-outline-danger btn-xs"
+                        >
+                          <i className="fa fa-trash" aria-hidden="true"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                </Fragment>
+              ))}
 
-    </div>
+              <div className="pull-right">
+                <Link to={'/catalogo'}>
+                  <button className="btn btn-outline-primary pull-right">
+                    Continuar comprando
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className="card-footer">
+              <div className="coupon col-md-5 col-sm-5 no-padding-left pull-left">
+                <div className="row">
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="cupone code"
+                    />
+                  </div>
+                  <div className="col-6">
+                    <input
+                      type="submit"
+                      className="btn btn-default"
+                      value="Use cupone"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="pull-right" style={{ margin: 10 }}>
+                <a href="" className="btn btn-primary pull-right">
+                  ORDEN
+                </a>
+                <div className="pull-right" style={{ margin: 5 }}>
+                  Total price:{' '}
+                  <b>
+                    $
+                    {carrito
+                      .map(p => p.payload.price * p.quantity)
+                      .reduce((a, b) => a + b, 0)
+                      .toFixed(2)}
+                  </b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+            )}
+
+    </Fragment>
 
   );
 }
