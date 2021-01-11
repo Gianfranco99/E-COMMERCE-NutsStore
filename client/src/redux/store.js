98755439d97022ad0686babb29e028fba8f65c40
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2021-01-11 10:09:13 -0300

    local storage sin andar

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,27 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/reducers";
 import thunk from "redux-thunk";
+//import {loadState, saveState} from "./localStorage.js"
 
-const store = createStore(
-    rootReducer,
+import { persistStore, persistReducer } from 'redux-persist' // imports from redux-persist
+import storage from 'redux-persist/lib/storage' 
+
+const persistConfig = { // configuration object for redux-persist
+  key: 'root',
+  storage, // define which storage to use
+  whitelist: ['productCart']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(
+  persistedReducer,
     compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
-  export default store;
+
+export const  persistor = persistStore(store)
+// store.subscribe( function () {
+//   saveState(store.getState())
+// })
+  
+  
