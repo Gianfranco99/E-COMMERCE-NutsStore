de1578fc3e5c35b53de4d16aeca362fff9325e3b
Harold Revelo <haroldrevelo@gmail.com>
2021-01-18 13:06:33 -0500

    Autenticación

--- client/src/components/MyUser/MiCuenta.jsx
+++ client/src/components/MyUser/MiCuenta.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import InicioDeSesion from "../Login/InicioDeSesion";
 import DashboardAdmin from "./DashboardAdmin";
 import MiPerfil from "./MiPerfil";
 
 function MiCuenta() {
-  const loggedIn = useSelector((state) => state.loggedIn);
-  const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.auth.user);
 
   return (
-    <>
-      {loggedIn && user.isAdmin && <DashboardAdmin />}
-      {loggedIn && !user.isAdmin && <MiPerfil />}
-      {!loggedIn && <InicioDeSesion />}
-    </>
+    <Fragment>
+      {user.isAdmin  ? 
+      (<DashboardAdmin /> ):(
+        <MiPerfil />
+      )}
+    </Fragment>
   );
 }
 
